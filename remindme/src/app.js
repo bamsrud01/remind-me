a0922c2a6aef291df15f474bc2e31733730d3bda
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-28 17:47:46 -0500

    Added basic structure to app.js

--- remindme/src/app.js
+++ remindme/src/app.js
@@ -1,7 +1,19 @@
 //  Import libraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
 //  Import components
 
 //  Create App component
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Remind Me</Text>
+      </View>
+    );
+  }
+}
 
 //  Export App component
+export default App;
