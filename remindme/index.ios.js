fca45b1c5fd04bdb5462e1a3dc90b341174a252b
Barrett Amsrud <barrettamsrud@gmail.com>
2017-04-28 17:40:21 -0500

    Directed both ios and android indexes to app.js

---
+++ remindme/index.ios.js
@@ -0,0 +1,6 @@
+//  Import root component and app registry
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+//  Register component
+AppRegistry.registerComponent('remindme', () => App);
