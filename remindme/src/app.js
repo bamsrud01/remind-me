9b0d342c5f33cc4a01bd5549925d573fc8c8705d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-07 19:23:25 -0500

    Added input field to application

--- remindme/src/app.js
+++ remindme/src/app.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 
 //  Import components
-import Header from './components/Header';
+import { Header, Input } from './components/common';
 
 //  Create App component
 class App extends Component {
+
+  state = {
+    task: ''
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Remind Me!" />
+        <Input
+          placeholder="Enter message here"
+          label="Task"
+          value={this.state.task}
+          onChangeText={task => this.setState({ task })}
+        />
       </View>
     );
   }
