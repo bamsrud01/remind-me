0ec8378657f4121c6410dee61079f6e89640066c
Barrett Amsrud <barrettamsrud@gmail.com>
2017-05-09 22:27:50 -0500

    Set styles, added welcome message

--- remindme/src/app.js
+++ remindme/src/app.js
@@ -1,21 +1,38 @@
 //  Import libraries
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 //  Create App component
 class App extends Component {
 
-  state = {
-    task: ''
-  }
-
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello!</Text>
       </View>
     );
   }
 }
+
+//  Create styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
 
 //  Export App component
 export default App;
